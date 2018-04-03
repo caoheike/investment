4fd96af6a647a26971fa1647edb1c08c9910512a
Administrator <Administrator@XB-201608131526>
2018-04-03 14:29:59 +0800

    整合登录，删除不需要的菜单。

--- src/main/java/com/howard/investment/controller/HomeController.java
+++ src/main/java/com/howard/investment/controller/HomeController.java
@@ -5,6 +5,7 @@ import com.howard.investment.tools.Tools;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -47,11 +48,42 @@ public class HomeController {
     }
     
     @RequestMapping("index.html")
-    public String index(HttpSession session){
+    public String index(HttpSession session,Model model){
     	Object sessionUser=session.getAttribute("sessionUser");
     	if(sessionUser==null){
     		return "login";
     	}
+    	Map map=(Map)sessionUser;
+    	model.addAttribute("userName", map.get("username").toString());
+    	if("10".equals(map.get("typeid").toString())){
+    		model.addAttribute("userLevel", "局用户");
+    	}else if("2".equals(map.get("typeid").toString())){
+    		model.addAttribute("userLevel", "部门用户");
+    	}else if("0".equals(map.get("typeid").toString())){
+    		model.addAttribute("userLevel", "超级管理员");
+    	}
+    	
         return "index";
+    }
+    @ResponseBody
+    @RequestMapping("userinfo")
+    public Map<String,Object> userinfo(HttpSession session){
+    	Map<String,Object> data=new HashMap<>();
+    	Object sessionUser=session.getAttribute("sessionUser");
+    	if(sessionUser==null){
+    		data.put("flag", false);
+    		return data;
+    	}
+    	Map map=(Map)sessionUser;
+    	data.put("userName", map.get("username").toString());
+    	if("10".equals(map.get("typeid").toString())){
+    		data.put("userLevel", "局用户");
+    	}else if("2".equals(map.get("typeid").toString())){
+    		data.put("userLevel", "部门用户");
+    	}else if("0".equals(map.get("typeid").toString())){
+    		data.put("userLevel", "超级管理员");
+    	}
+    	data.put("flag", true);
+        return data;
     }
 }
